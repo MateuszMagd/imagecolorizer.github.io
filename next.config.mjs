6c00d615e1e0845a731d674fed6aa2e71136767e
MateuszMagd <240742@edu.p.lodz.pl>
2024-06-13 01:26:25 +0200

    Changes in configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
     output: 'export',
     distDir: 'out',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/nazwa-repozytorium' : '',
   };
   
   export default nextConfig;
